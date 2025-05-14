09b5176fb9be0118288664b8276e141f0dc7cd98
VITOR PAIVA NUNES DE PAULA <vitor.paula19@etec.sp.gov.br>
2025-05-14 12:12:14 -0300

    Coding

--- mu_e_muv/src/models/Movimento.java
+++ mu_e_muv/src/models/Movimento.java
@@ -3,6 +3,7 @@ package models;
 public abstract class Movimento {
     private double velocidade;
     private double distancia;
+    private double tempo;
 
     public Movimento(double velocidade, double distancia){
         this.velocidade = velocidade;
@@ -16,10 +17,14 @@ public abstract class Movimento {
     public void setDistancia(double distancia){
         this.distancia = distancia;
     }
+    public void setTempo(double tempo){
+        this.tempo = tempo;
+    }
 
     //Métodos get
     public double getVelocidade(){return velocidade;}
     public double getDistancia(){return distancia;}
+    public double getTempo(){return tempo;}
 
     public void explicarMovimento(){
         System.out.println("O movimento consiste numa mudança de posição de um corpo ou de um sistema, em relação ao tempo, quando medido por um dado observador\n num referencial determinado. Só se pode medir o movimento relativo. O movimento absoluto não possui significado.");
--- mu_e_muv/src/models/Movimento_Uniforme.java
+++ mu_e_muv/src/models/Movimento_Uniforme.java
@@ -1,5 +1,12 @@
 package models;
 
-public class Movimento_Uniforme {
-    
+public class Movimento_Uniforme extends Movimento {
+    public Movimento_Uniforme(double velocidade, double distancia){
+        super(velocidade, distancia);
+    }
+
+    @Override
+    public void explicarMovimentoEspecifico(){
+        System.out.println("Movimento uniforme é a variação do espaço de um objeto quando a velocidade dele não varia ");
+    }
 }
