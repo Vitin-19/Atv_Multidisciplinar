198a9d0ece974cf71b00f1b2e07fcfab1afae376
Vitor Paiva Nunes de Paula <brdepaulav@gmail.com>
2025-05-19 10:08:52 -0300

    Delete mu_e_muv directory

--- mu_e_muv/src/models/Formulas_MU.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public interface Formulas_MU {
-    public double velocidadeMedia(double deltaS, double deltaV);
-    public double calcular_distancia(double velocidade, double tempo );
-    public double calcular_tempo(double espaco, double velocidade);
-}
--- mu_e_muv/src/models/Formulas_MUV.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public interface Formulas_MUV {
-    public double sorvetao(double v0, double s0, double aceleracao, double tempo);
-    public double voat(double v0, double aceleracao, double tempo);
-    public double torricelli(double v0, double deltaS, double aceleracao);
-}
