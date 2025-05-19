cb7c2d40dec88c04088cd7d2cc8cb9349b0d7564
VITOR PAIVA NUNES DE PAULA <vitor.paula19@etec.sp.gov.br>
2025-05-19 10:09:47 -0300

    Adding Botanica

---
+++ Botanica/src/models/Planta.java
@@ -0,0 +1,9 @@
+package models;
+
+public abstract class Planta {
+    private String especie;
+    private String tipo;
+    private String nome_cientifico;
+    private String classe;
+    private String familia; 
+}
